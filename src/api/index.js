28337836944ed9d33c63b8618a52a4a875d0b488
Luda Modna <ludmilamodna3@gmail.com>
2025-07-20 10:58:36 +0300

    Add axios

---
+++ src/api/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+// axios
+//   .get('https://randomuser.me/api/?results=2')
+//   .then(data => console.log(data.data))
+//   .catch(e => console.log(e));
+
+const axiosInstance = axios.create({
+  baseURL: 'https://randomuser.me/api',
+});
+
+axiosInstance
+  .get('/?results=2')
+  .then(({ data }) => console.log(data))
+  .catch(err => console.log(err));
+
+axiosInstance
+  .get('/?gender=female')
+  .then(({ data }) => console.log(data))
+  .catch(err => console.log(err));
